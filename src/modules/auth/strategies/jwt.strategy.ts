60340eca5d0556d9bc4d6dde304f804a351b81f7
Dandi Kurnia <dandikurnia@gmail.com>
2026-03-20 19:11:48 +0700

    guard auth

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,48 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+
+type JwtPayload = {
+  sub: number;
+  email: string;
+  roleId: number;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET_KEY ?? 'secretKey',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+      include: {
+        role: {
+          include: {
+            rolePermissions: {
+              include: {
+                permission: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      roleId: user.roleId,
+      role: user.role,
+    };
+  }
+}
